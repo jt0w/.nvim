c64512e7f288633dd7140893387feb43bd133012
secretval <108518296+SecretVal@users.noreply.github.com>
2025-02-14 18:50:31 +0100

    tree-sitter

--- config/lua/custom/plugins/treesitter.lua
+++ config/lua/custom/plugins/treesitter.lua
@@ -1,14 +1,15 @@
 return {
     "nvim-treesitter",
+    lazy = false,
     before = function()
         deps.add { source = "nvim-treesitter/nvim-treesitter" }
     end,
     after = function()
-    require'nvim-treesitter.configs'.setup {
-        ensure_installed = "all",
-        auto_install = true,
-        highlight = { ennable = true },
-        indent = { ennable = true },
-    }
+        require 'nvim-treesitter.configs'.setup {
+            ensure_installed = "all",
+            auto_install = true,
+            highlight = { ennable = true },
+            indent = { ennable = true },
+        }
     end
 }
