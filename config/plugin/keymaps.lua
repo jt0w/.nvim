3024d897a6cb67a7db9d046598a1aca8526ed5f5
secretval <108518296+SecretVal@users.noreply.github.com>
2025-02-15 15:27:24 +0100

    keymaps

--- config/plugin/keymaps.lua
+++ config/plugin/keymaps.lua
@@ -3,10 +3,15 @@ local s = vim.keymap.set
 s("n", "<leader>e", "<cmd>Explore<cr>")
 s("n", "<esc>", "<cmd>noh<cr>")
 
+s("n", "<c-j>", "<c-w><c-j>")
+s("n", "<c-k>", "<c-w><c-k>")
+s("n", "<c-l>", "<c-w><c-l>")
+s("n", "<c-h>", "<c-w><c-h>")
+
 s("n", "<leader>o", "<cmd>copen<cr>")
 s("n", "<leader>c", "<cmd>cclose<cr>")
-s("n", "<C-j>", "<cmd>cnext<cr>")
-s("n", "<C-k>", "<cmd>cprev<cr>")
+s("n", "<C-shift-j>", "<cmd>cnext<cr>")
+s("n", "<C-shift-k>", "<cmd>cprev<cr>")
 
 -- Brilliant keymaps i stole from https://github.com/ThePrimeagen/init.lua
 s("v", "K", ":m '<-2<CR>gv=gv")
