442cba254a8a59da81a9063b5797de39ac68aff8
secretval <108518296+SecretVal@users.noreply.github.com>
2025-02-23 14:55:06 +0100

    clue

--- config/lua/custom/plugins/mini.lua
+++ config/lua/custom/plugins/mini.lua
@@ -1,6 +1,9 @@
 return {
     "mini.nvim",
     after = function()
-        require("mini.ai").setup()
+        require("mini.align").setup()
+        require("mini.operators").setup()
+        require("mini.surround").setup()
+        require("mini.clue").setup()
     end,
 }
