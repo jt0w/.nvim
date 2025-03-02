2605818c910d91584aa1e20ed9f73717786f9a8b
secretval <108518296+SecretVal@users.noreply.github.com>
2025-03-02 22:30:44 +0100

    telescope

--- config/lua/custom/plugins/snacks.lua
+++ config/lua/custom/plugins/snacks.lua
@@ -16,14 +16,9 @@ return {
         inputs       = { enabled = true },
     },
     keys = {
-        { "<leader>f",  function() Snacks.picker.files()   end, desc = "Find Files",     mode = { "n", "t" } },
-        { "<leader>b",  function() Snacks.picker.buffers() end, desc = "Find Buffers",                       },
-        { "<leader>h",  function() Snacks.picker.help()    end, desc = "Help Pages"                          },
-        { "<leader>s",  function() Snacks.picker.grep()    end, desc = "Grep",                               },
-        { "<leader>m",  function() Snacks.picker.man()     end, desc = "Man Pages",                          },
-        { "<C-,>",      function() Snacks.terminal()       end, desc = "Open Terminal",  mode = { "n", "t" } },
-        { "<C-.>",      function() Snacks.scratch()        end, desc = "Open Scratch Buffer",                },
-        { "<leader>go", function() Snacks.gitbrowse()      end, desc = "Open git project in browser"         },
+        { "<C-,>",      function() Snacks.terminal() end,  desc = "Open Terminal",              mode = { "n", "t" } },
+        { "<C-.>",      function() Snacks.scratch() end,   desc = "Open Scratch Buffer", },
+        { "<leader>go", function() Snacks.gitbrowse() end, desc = "Open git project in browser" },
     },
     config = function()
         Snacks.toggle.dim():map("<leader>tD")
