e646bedefeee90328a6d18376a0a2f6a7049f8e1
secretval <108518296+SecretVal@users.noreply.github.com>
2025-03-12 15:09:29 +0100

    neorg

--- config/lua/custom/plugins/neorg.lua
+++ config/lua/custom/plugins/neorg.lua
@@ -1,7 +1,10 @@
 return {
     "nvim-neorg/neorg",
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
+    keys = {
+        { "<leader>N", "<cmd>Neorg<cr>", desc = "Neorg" }
+    },
     opts = {
         load = {
             ["core.defaults"] = {},
