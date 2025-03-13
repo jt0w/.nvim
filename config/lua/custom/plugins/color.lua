12f887138e4f32e1a205fa2384e792b9a37a4b03
secretval <108518296+SecretVal@users.noreply.github.com>
2025-03-13 14:57:47 +0100

    melgne

--- config/lua/custom/plugins/color.lua
+++ config/lua/custom/plugins/color.lua
@@ -1,8 +1,8 @@
 return {
-    "blazkowolf/gruber-darker.nvim",
+    "savq/melange-nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme "gruber-darker"
+        vim.cmd.colorscheme "melagne"
     end
 }
