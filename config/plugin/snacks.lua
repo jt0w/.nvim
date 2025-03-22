46e41c684570c293cefcd8a26962bcd404df1c5a
secretval <108518296+SecretVal@users.noreply.github.com>
2025-03-22 12:32:13 +0100

    termi

--- config/plugin/snacks.lua
+++ config/plugin/snacks.lua
@@ -8,5 +8,5 @@ Snacks.setup {
 
 vim.keymap.set('n', '<leader>m', function() Snacks.picker.man() end, {desc = "Snacks man pages"});
 vim.keymap.set('n', '<leader>n', function() Snacks.picker.notifications() end, {desc = "Snacks man pages"});
-vim.keymap.set('n', '<leader>t', function() Snacks.terminal() end, {desc = "Snacks terminal"});
+vim.keymap.set('n', '<leader>tt', function() Snacks.terminal() end, {desc = "Snacks terminal"});
 Snacks.toggle.option("wrap"):map("<leader>tw")
