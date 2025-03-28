0a3e3fa0bccf6a6fb75d0a43c8f21e2b2044e154
secretval <108518296+SecretVal@users.noreply.github.com>
2025-03-28 16:32:46 +0100

    term

--- config/plugin/markdown.lua
+++ config/plugin/markdown.lua
@@ -2,4 +2,3 @@ require('render-markdown').setup({})
 vim.g.table_mode_corner = '|'
 vim.keymap.set("n", "<leader>tm", "<cmd>TableModeToggle<cr>")
 vim.keymap.set("n", "<leader>tr", "<cmd>TableModeRealign<cr>")
-vim.keymap.set("n", "<leader>ti", "<cmd>Tableize<cr>")
