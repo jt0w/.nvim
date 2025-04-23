31d3a36a9b87f137191da2adcdcd6acfc1019b59
secretval <108518296+SecretVal@users.noreply.github.com>
2025-04-23 16:30:52 +0200

    java

--- config/plugin/lsp.lua
+++ config/plugin/lsp.lua
@@ -6,36 +6,38 @@ local servers = {
     gopls = true,
     ccls = true,
     nil_ls = true,
+    jdtls = true,
 }
-
-local on_attach = function(client, bufnr) 
-    if client.server_capabilities.inlayHintProvider then
-        vim.lsp.inlay_hint.enable(true)
-        Snacks.toggle.inlay_hints():map("<leader>th")
-    end
-    Snacks.toggle.diagnostics():map("<leader>td")
-
-    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
-    vim.keymap.set("i", "<C-space>", "<C-x><C-o>", { buffer = bufnr })
-
-
-    vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end, { buffer = bufnr })
-    vim.keymap.set("n", "gD", function() Snacks.picker.lsp_declarations() end, { buffer = bufnr })
-    vim.keymap.set("n", "gr", function() Snacks.picker.lsp_references() end, { buffer = bufnr })
-    vim.keymap.set("n", "gi", function() Snacks.picker.lsp_implementations() end, { buffer = bufnr })
-
-    vim.keymap.set("n", "<leader>wd", function() Snacks.picker.lsp_symbols() end, { buffer = bufnr })
-    vim.keymap.set("n", "<leader>wD", function() Snacks.picker.lsp_workspace_symbols() end, { buffer = bufnr })
-
-    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = bufnr })
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
-end
 
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
-        on_attach(assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client"), args.bufnr)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client") 
+        local bufnr = args.bufnr
+        if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(true)
+            Snacks.toggle.inlay_hints():map("<leader>th")
+        end
+        Snacks.toggle.diagnostics():map("<leader>td")
+
+        vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+        vim.keymap.set("i", "<C-space>", "<C-x><C-o>", { buffer = bufnr })
+
+
+        vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end, { buffer = bufnr })
+        vim.keymap.set("n", "gD", function() Snacks.picker.lsp_declarations() end, { buffer = bufnr })
+        vim.keymap.set("n", "gr", function() Snacks.picker.lsp_references() end, { buffer = bufnr })
+        vim.keymap.set("n", "gi", function() Snacks.picker.lsp_implementations() end, { buffer = bufnr })
+
+        vim.keymap.set("n", "<leader>wd", function() Snacks.picker.lsp_symbols() end, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>wD", function() Snacks.picker.lsp_workspace_symbols() end, { buffer = bufnr })
+
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
     end,
 })
+
+require("java").setup()
+
 for name, config in pairs(servers) do
     if config == true then
         config = {}
--- lib/neovim.nix
+++ lib/neovim.nix
@@ -5,76 +5,90 @@
   config_path,
   pkgs,
   lib,
+  buildFHSEnv,
+  writeShellScript,
   ...
 }: let
-  config = let
-    extraPackages = with pkgs; [
-      rust-analyzer
-      ccls
-      gdb
-      zls
-      gopls
-      nil
-      jdt-language-server
-    ];
-    plugins = with pkgs.vimPlugins; [
-      nvim-treesitter.withAllGrammars
-      vim-nix
-      telescope-nvim
-      snacks-nvim
-      neorg
-      render-markdown-nvim
-      vim-table-mode
-      neocord
-      mini-nvim
-
-      vim-fugitive
-      gitsigns-nvim
-      octo-nvim
-
-      direnv-vim
-      nvim-ufo
-
-      oil-nvim
-
-      # lsp
-      nvim-lspconfig
-      fidget-nvim
-      nvim-jdtls
-
-      # debugging / dap
-      nvim-dap
-      nvim-dap-ui
-      nvim-nio
-      nvim-dap-virtual-text
-
-      blink-cmp
-
-      luasnip
-      friendly-snippets
-
-      catppuccin-nvim
-    ];
-  in
-    neovimUtils.makeNeovimConfig {
-      inherit extraPackages;
-      inherit plugins;
-      vimAlias = true;
-      viAlias = true;
-
-      customRC = ''
-        set runtimepath^=${config_path}
-        set runtimepath^=${config_path}/after
-        source ${config_path + "/init.lua"}
-      '';
-    }
-    // {
-      wrapperArgs = [
-        "--prefix"
-        "PATH"
-        ":"
-        "${lib.makeBinPath extraPackages}"
+  nvim = let
+    config = let
+      extraPackages = with pkgs; [
+        rust-analyzer
+        ccls
+        gdb
+        zls
+        gopls
+        nil
+        jdt-language-server
       ];
-    };
+      plugins = with pkgs.vimPlugins; [
+        nvim-treesitter.withAllGrammars
+        vim-nix
+        telescope-nvim
+        snacks-nvim
+        neorg
+        render-markdown-nvim
+        vim-table-mode
+        neocord
+        mini-nvim
+
+        vim-fugitive
+        gitsigns-nvim
+        octo-nvim
+
+        direnv-vim
+        nvim-ufo
+
+        oil-nvim
+
+        # lsp
+        nvim-lspconfig
+        fidget-nvim
+        mason-nvim
+        nvim-java
+
+        # debugging / dap
+        nvim-dap
+        nvim-dap-ui
+        nvim-nio
+        nvim-dap-virtual-text
+
+        blink-cmp
+
+        luasnip
+        friendly-snippets
+
+        catppuccin-nvim
+      ];
+    in
+      neovimUtils.makeNeovimConfig {
+        inherit extraPackages;
+        inherit plugins;
+        vimAlias = true;
+        viAlias = true;
+
+        customRC = ''
+          set runtimepath^=${config_path}
+          set runtimepath^=${config_path}/after
+          source ${config_path + "/init.lua"}
+        '';
+      }
+      // {
+        wrapperArgs = [
+          "--prefix"
+          "PATH"
+          ":"
+          "${lib.makeBinPath extraPackages}"
+        ];
+      };
+  in
+    wrapNeovimUnstable neovim-unwrapped config;
 in
-  wrapNeovimUnstable neovim-unwrapped config
+  buildFHSEnv {
+    name = "nvim";
+    targetPkgs = pkgs: [
+      nvim
+    ];
+    runScript = writeShellScript "run-nvim.sh" ''
+      exec ${nvim}/bin/nvim "$@"
+    '';
+  }
