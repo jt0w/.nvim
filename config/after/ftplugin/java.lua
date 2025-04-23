dd73675888d0241ddb82b39c38499c74852c4163
secretval <108518296+SecretVal@users.noreply.github.com>
2025-04-23 16:46:30 +0200

    set data

--- config/after/ftplugin/java.lua
+++ config/after/ftplugin/java.lua
@@ -1,5 +1,10 @@
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = '~/.jdt-cache/' .. project_name
 local config = {
-    cmd = {vim.g.jdt},
+    cmd = {
+        vim.g.jdt,
+        '-data' .. workspace_dir,
+    },
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
