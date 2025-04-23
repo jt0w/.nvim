a489c389a5dbafd837fb5f427659aed2e535162d
secretval <108518296+SecretVal@users.noreply.github.com>
2025-04-23 21:05:42 +0200

    try: making java simpler

--- config/after/ftplugin/java.lua
+++ config/after/ftplugin/java.lua
@@ -1,20 +1,17 @@
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = vim.fn.expand('~/.jdt-cache/' .. project_name)
+local workspace_dir = '~/.jdt-cache/' .. project_name
 local config = {
     cmd = {
         vim.g.jdt,
         '-data' .. workspace_dir,
     },
-    init_options = {
-        bundles = {}, -- Add any debugger/test support jars here if needed
-    },
     settings = {
         java = {
             configuration = {
-                updateBuildConfiguration = "interactive", -- <- crucial for Gradle auto-import
+                updateBuildConfiguration = "automatic", -- <- crucial for Gradle auto-import
             },
         },
     },
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
+
 require('jdtls').start_or_attach(config)
