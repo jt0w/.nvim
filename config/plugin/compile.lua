d334e3024a3d7d565c9c82c7dbbc8f869d74fc17
secretval <108518296+SecretVal@users.noreply.github.com>
2025-06-28 22:13:17 +0200

    I think I can finally compile stuff

--- config/plugin/compile.lua
+++ config/plugin/compile.lua
@@ -1,16 +1,10 @@
--- cgetexpr system("./make vism examples/example.vism")
---
-
 Command = nil
-
 vim.api.nvim_create_user_command(
     'Compile',
     function()
         vim.ui.input({ prompt = "Command: " }, function(input)
-            vim.cmd("cgetexpr ('" .. input .. "')");
+            vim.cmd("cexpr system('" .. input .. "')");
             Command = input
-            vim.cmd("make")
-            vim.cmd("copen")
         end)
     end,
     {nargs = 0}
@@ -19,12 +13,10 @@ vim.api.nvim_create_user_command(
 vim.api.nvim_create_user_command(
     'Recompile',
     function()
-        vim.cmd("cgetexpr ('" .. Command .. "')");
-        vim.cmd("make");
-        vim.cmd("copen");
+        vim.cmd("cexpr system('" .. Command .. "')");
     end,
     {nargs = 0}
 )
 
 vim.keymap.set("n", "<leader>cC", "<cmd>Compile<cr>");
-vim.keymap.set("n", "<leader>cc", "<cmd>Recompile<cr>");
+vim.keymap.set("n", "<leader>cc", "<cmd>make<cr>");
