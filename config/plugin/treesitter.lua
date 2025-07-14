af587af4dabdd3a0950f31745374699fd87c1d03
secretval <108518296+SecretVal@users.noreply.github.com>
2025-07-14 15:04:05 +0200

    why not

--- config/plugin/treesitter.lua
+++ config/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { '<filetype>' },
+  pattern = { '*' },
   callback = function()
-    vim.treesitter.start() 
+    vim.treesitter.start()
     vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
