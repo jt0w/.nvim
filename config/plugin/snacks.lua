1f318cc02b2956a653e77cfec145929c345d1f83
secretval <108518296+SecretVal@users.noreply.github.com>
2025-07-14 13:08:00 +0200

    feat(snacks): toggle diagnostics

--- config/plugin/snacks.lua
+++ config/plugin/snacks.lua
@@ -11,3 +11,4 @@ vim.keymap.set("n", "<leader>m", function() Snacks.picker.man() end, {desc = "Sn
 vim.keymap.set("n", "<C-,>", function() Snacks.terminal() end, {desc = "Snacks terminal"});
 vim.keymap.set({"n", "v"}, "<leader>go", function() Snacks.gitbrowse() end, {desc = "Git open"});
 Snacks.toggle.option("wrap"):map("<leader>tw")
+Snacks.toggle.diagnostics():map("<leader>td");
