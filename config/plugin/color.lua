e90cd7f4ba548327e464c1d2cbc612d81c999e69
secretval <108518296+SecretVal@users.noreply.github.com>
2025-07-29 12:27:11 +0200

    feat(color): mini.pick transparent

--- config/plugin/color.lua
+++ config/plugin/color.lua
@@ -1,4 +1,13 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "MiniPickNormal", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "MiniPickBorder", { bg = "NONE" })
+  end,
+})
+
 require("catppuccin").setup {
-  flavour = "mocha"
+  flavour = "mocha",
 }
+
 vim.cmd.colorscheme("catppuccin")
