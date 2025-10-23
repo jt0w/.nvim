3f3ac2a4e9c8b5aed948279cb47c8d5e3f470267
secretval <108518296+SecretVal@users.noreply.github.com>
2025-10-23 16:24:38 +0200

    lets try this

--- config/init.lua
+++ config/init.lua
@@ -143,8 +143,8 @@ require("luasnip").setup({
 require("luasnip.loaders.from_lua").load({ paths = { vim.g.snippets } })
 local ls = require "luasnip"
 
-vim.keymap.set({ "i", "s" }, "<c-k>", function() return ls.expand_or_jump() end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<c-j>", function() return ls.jump(-1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<c-f>", function() return ls.expand_or_jump() end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<c-s>", function() return ls.jump(-1) end, { silent = true })
 
 vim.lsp.enable({
     "rust_analyzer", "ccls", "zls", "gopls",
