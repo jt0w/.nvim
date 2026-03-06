b831faac8cfa1973437f53dabb9fbc6b05425a5e
secretval <108518296+SecretVal@users.noreply.github.com>
2026-03-06 15:23:22 +0100

    hipatterns

--- config/init.lua
+++ config/init.lua
@@ -112,6 +112,17 @@ require("mini.align").setup()
 require("mini.splitjoin").setup()
 require("mini.surround").setup()
 
+local hipatterns = require('mini.hipatterns')
+hipatterns.setup({
+  highlighters = {
+    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+    hex_color = hipatterns.gen_highlighter.hex_color(),
+  },
+})
+
 local tele = require("telescope")
 local builtin = require("telescope.builtin")
 
